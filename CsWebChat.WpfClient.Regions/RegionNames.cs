53ff3505a8e6d0b458bb20a4c64ebb27e21f609f
P H <ph1387@t-online.de>
2018-08-15 22:36:08 +0200

    Added basic wpf prism client projects to solution

---
+++ CsWebChat.WpfClient.Regions/RegionNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsWebChat.WpfClient.Regions
+{
+    public class RegionNames
+    {
+        public static readonly string MAIN_REGION = "MainRegion";
+    }
+}
---
+++ CsWebChat.WpfClient/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CsWebChat.WpfClient
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (MainWindow)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
