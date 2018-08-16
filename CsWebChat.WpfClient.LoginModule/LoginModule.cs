dc779bfb1da0a4ae1985a8c959ba6ebc909fb30d
P H <ph1387@t-online.de>
2018-08-16 05:59:07 +0200

    Added basic login module to solution

---
+++ CsWebChat.WpfClient.LoginModule/LoginModule.cs
@@ -0,0 +1,47 @@
+﻿using CsWebChat.WpfClient.LoginModule.Views;
+using CsWebChat.WpfClient.Regions;
+using Microsoft.Practices.Unity;
+using Prism.Logging;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsWebChat.WpfClient.LoginModule
+{
+    public class LoginModule : IModule
+    {
+        private readonly IUnityContainer _container;
+        private readonly IRegionManager _regionManager;
+
+        public LoginModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            if (container == null || regionManager == null)
+                throw new ArgumentException();
+
+            this._container = container;
+            this._regionManager = regionManager;
+        }
+
+
+        public void Initialize()
+        {
+            this.InitializeServices();
+            this.InitializeRegions();
+        }
+
+        private void InitializeServices()
+        {
+            
+        }
+
+        private void InitializeRegions()
+        {
+            this._regionManager.RegisterViewWithRegion(RegionNames.MAIN_REGION, typeof(LoginView));
+            this._regionManager.RegisterViewWithRegion(RegionNames.MAIN_REGION, typeof(RegisterView));
+        }
+    }
+}
---
+++ CsWebChat.WpfClient.LoginModule/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Events;
+using Prism.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsWebChat.WpfClient.LoginModule.ViewModels
+{
+    class RegisterViewModel
+    {
+        private readonly IUnityContainer _container;
+        private readonly IEventAggregator _eventAggregator;
+        private readonly ILoggerFacade _logger;
+
+        public RegisterViewModel(IUnityContainer container, IEventAggregator eventAggregator, ILoggerFacade logger)
+        {
+            if (container == null || eventAggregator == null || logger == null)
+                throw new ArgumentException();
+
+            _container = container;
+            _eventAggregator = eventAggregator;
+            _logger = logger;
+        }
+    }
+}
