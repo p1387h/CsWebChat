45e12c31fc653130358c2acab52a1236a4945e24
P H <ph1387@t-online.de>
2018-08-28 07:08:35 +0200

    Added SignalR.Client library to necessary projects, made ChatModule provide factory for generating HubConnections

--- CsWebChat.WpfClient.ChatModule/ChatModule.cs
+++ CsWebChat.WpfClient.ChatModule/ChatModule.cs
@@ -1,11 +1,14 @@
 ﻿using CsWebChat.WpfClient.ChatModule.Views;
 using CsWebChat.WpfClient.Regions;
+using CsWebChat.WpfClient.WebLogicModule.Models;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Practices.Unity;
 using Prism.Modularity;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,12 +36,54 @@ namespace CsWebChat.WpfClient.ChatModule
 
         private void InitializeServices()
         {
+            this._container.RegisterType<HubConnection>(new InjectionFactory(this.HubConnectionFactoryFunction));
+
             // Views registered for the possibility of INavigationAware returning false and
             // the RegionManager therefore requiring a new instance. The name is needed since
             // a Uri is used for resolving the type when navigating.
             this._container.RegisterType<object, ChatSplitView>(nameof(ChatSplitView));
             this._container.RegisterType<object, UserView>(nameof(UserView));
             this._container.RegisterType<object, ChatView>(nameof(ChatView));
+        }
+
+        private HubConnection HubConnectionFactoryFunction(IUnityContainer container)
+        {
+            var addressStorage = container.Resolve<AddressStorage>();
+            var authenticationStorage = container.Resolve<AuthenticationStorage>();
+            var antiforgeryStorage = container.Resolve<AntiforgeryStorage>();
+
+            // Domain has to be set in order to prevent ArgurmentExceptions when adding
+            // the Cookies.
+            var target = new Uri(addressStorage.ServerAddress);
+            var domain = target.Host;
+            var authenticationCookie = new Cookie(authenticationStorage.AuthenticationCookieName,
+                authenticationStorage.AuthenticationCookieContent)
+            {
+                Domain = domain
+            };
+            var antiforgeryCookie = new Cookie(antiforgeryStorage.AntiforgeryCookieName,
+                antiforgeryStorage.AntiforgeryCookieContent)
+            {
+                Domain = domain
+            };
+
+            // The address of the server could may already end with a "/". In this case
+            // another one is not needed.
+            var combiner = (addressStorage.ServerAddress.EndsWith("/")) ? "" : "/";
+            var address = String.Join(combiner, addressStorage.ServerAddress, "ws");
+
+            // Ensure that the correct connection to the websocket is made and is always present 
+            // when requesting an instance.
+            var connection = new HubConnectionBuilder()
+                .WithUrl(address, (options) =>
+                {
+                    options.Cookies.Add(authenticationCookie);
+                    options.Cookies.Add(antiforgeryCookie);
+                    options.Headers.Add(antiforgeryStorage.CsrfHeader, antiforgeryStorage.CsrfToken);
+                })
+                .Build();
+
+            return connection;
         }
 
         private void InitializeViews()
--- CsWebChat.WpfClient.ChatModule/ViewModels/ChatSplitViewModel.cs
+++ CsWebChat.WpfClient.ChatModule/ViewModels/ChatSplitViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CsWebChat.WpfClient.ChatModule.Views;
 using CsWebChat.WpfClient.Regions;
+using Microsoft.AspNetCore.SignalR.Client;
 using Prism.Events;
 using Prism.Logging;
 using Prism.Mvvm;
@@ -14,37 +15,46 @@ namespace CsWebChat.WpfClient.ChatModule.ViewModels
 {
     class ChatSplitViewModel : BindableBase, INavigationAware
     {
+        // Set as RegionContext in order to make the connection available to all 
+        // other hostet child views.
+        public HubConnection ConnectionRegionContext { get { return this._connection; } }
+
         private readonly IRegionManager _regionManager;
         private readonly IEventAggregator _eventAggregator;
         private readonly ILoggerFacade _logger;
+        private readonly HubConnection _connection;
 
         public ChatSplitViewModel(IRegionManager regionManager, IEventAggregator eventAggregator,
-            ILoggerFacade logger)
+            ILoggerFacade logger, HubConnection connection)
         {
             if (regionManager == null || eventAggregator == null
-                || logger == null)
+                || logger == null || connection == null)
                 throw new ArgumentException();
 
             this._regionManager = regionManager;
             this._eventAggregator = eventAggregator;
             this._logger = logger;
+            this._connection = connection;
         }
 
 
         // INavigationAware:
-        public async void OnNavigatedTo(NavigationContext navigationContext)
+        public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            
+            Task.Run(async () => { await this._connection.StartAsync(); });
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
-            return true;
+            // Always return false in order to force a new instantiation.
+            // -> Injecting a new HubConnection is necessary since the address might
+            // change.
+            return false;
         }
 
-        public async void OnNavigatedFrom(NavigationContext navigationContext)
+        public void OnNavigatedFrom(NavigationContext navigationContext)
         {
-            
+            Task.Run(async () => { await ConnectionRegionContext.StopAsync(); });
         }
     }
 }
